e65897a9e5539c7d455f21341250de4677e76f4f
Felipe Pêpe <felipepepe21@gmail.com>
2025-01-20 14:57:36 -0300

    feat: :lipstick: updtaed footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,5 @@
 import { Flex, GridItem, Text } from "@chakra-ui/react";
+import { BsYoutube } from "react-icons/bs";
 
 const Footer = () => {
   return (
@@ -6,14 +7,37 @@ const Footer = () => {
       colSpan={2}
       colStart={2}
       textColor={"white"}
-      fontSize={["xs"]}
+      fontSize={["xs", "xs", "sm"]}
       mt={[8]}
       mb={[3]}
+      textAlign={"center"}
     >
-      <Flex alignItems={"center"} flexDir={"column"} gap={[6]}>
-        <Flex alignItems={"center"} flexDir={"column"} gap={[2]}>
-          <Text>TERMOS DE SERVIÇO</Text>
-          <Text>CONTATO</Text>
+      <Flex alignItems={"center"} flexDir={"column"} gap={[6, 12]} w={"full"}>
+        <Flex
+          w={"full"}
+          alignItems={"center"}
+          justifyContent={"space-between"}
+          flexDir={["column", "row"]}
+          gap={[2]}
+        >
+          <Flex
+            w={["1.5rem", "1.5rem", "2rem"]}
+            h={["1.5rem", "1.5rem", "2rem"]}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            <BsYoutube style={{ width: "100%", height: "100%" }} />
+          </Flex>
+          <Flex
+            w={["100%", "50%", "40%", "30%"]}
+            alignItems={"center"}
+            justifyContent={["center", "space-between"]}
+            flexDir={["column", "row"]}
+            gap={[2]}
+          >
+            <Text>TERMOS DE SERVIÇO</Text>
+            <Text>CONTATO</Text>
+          </Flex>
         </Flex>
         <Text>Criado por Felipe Pêpe</Text>
       </Flex>
