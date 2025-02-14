235889c082812f9309f0c32dccd7b9de5ad4df1a
Felipe Pêpe <felipepepe21@gmail.com>
2025-02-14 11:51:48 -0300

    fix: :bug: fixed match json bug

--- src/utils/match/getMatchJson.ts
+++ src/utils/match/getMatchJson.ts
@@ -2,21 +2,19 @@ import { Match, PlayerGameData } from "@/types/match";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 function getMatchJson(match: any): Match {
-  const players: PlayerGameData[] = match.playersData
-    .filter((p: any) => p.total > 0)
-    .map((p: any) => {
-      return {
-        id: p.player._id,
-        name: p.player.name,
-        role: p.player.role,
-        avatar: p.player.avatar,
-        total: p.total,
-        chosen: p.chosen,
-        guess: p.guess,
-        revealed: p.revealed,
-        position: p.position,
-      };
-    });
+  const players: PlayerGameData[] = match.playersData.map((p: any) => {
+    return {
+      id: p.player._id,
+      name: p.player.name,
+      role: p.player.role,
+      avatar: p.player.avatar,
+      total: p.total,
+      chosen: p.chosen,
+      guess: p.guess,
+      revealed: p.revealed,
+      position: p.position,
+    };
+  });
 
   return {
     id: match._id,
